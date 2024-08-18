458f5b184e7ef9c7ea4021d8e76f61b81beac913
Lee Chau <leechou1993@gmail.com>
2024-08-18 23:31:51 +0930

    Adding Nav-Bar and Product Interface

---
+++ client/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class NavBarComponent { }
