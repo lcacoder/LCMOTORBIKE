8e9ace7d7cfb089aa196b9b1ae77f2907cbeb224
Lee Chau <leechou1993@gmail.com>
2024-08-31 15:00:03 +0930

    Set up "basket" functionality

---
+++ Core/Interface/IBasketRepository.cs
@@ -0,0 +1,11 @@
+using Core.Entities;
+
+namespace Core.Interface
+{
+    public interface IBasketRepository
+    {
+        Task<CustomerBasket> GetBasketAsync(string basketId);
+        Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket);
+        Task<bool> DeleteBasketAsync(string basketId);
+    }
+}
